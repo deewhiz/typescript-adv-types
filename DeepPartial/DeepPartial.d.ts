3947f209af02aba8e8b575040d30bd314631c447
Dustin Cook <dustin@libglug.org>
2019-12-30 23:06:23 -0800

    make Deep types deeper for more types

--- DeepPartial/DeepPartial.d.ts
+++ DeepPartial/DeepPartial.d.ts
@@ -3,9 +3,10 @@
  */
 export type DeepPartial<T> = {
     [P in keyof T]?:
-        T[P] extends Function ? T[P] :
-        T[P] extends Array<infer U> ? Array<DeepPartial<U>> :
-        T[P] extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>> :
+        T[P] extends Promise<infer R> ? Promise<DeepPartial<R>> :
+        T[P] extends Set<infer U> | ReadonlySet<infer U> ? ReadonlySet<DeepPartial<U>> :
+        T[P] extends Map<infer K, infer V> | ReadonlyMap<infer K, infer V> ? ReadonlyMap<K, DeepPartial<V>> :
+        T[P] extends Array<infer U> | ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>> :
         T[P] extends {} ? DeepPartial<T[P]> :
         T[P];
 }
--- DeepReadonly/DeepReadonly.d.ts
+++ DeepReadonly/DeepReadonly.d.ts
@@ -3,9 +3,10 @@
  */
 export type DeepReadonly<T> = {
     readonly [P in keyof T]:
-        T[P] extends Function ? T[P] :
-        T[P] extends Array<infer U> ? ReadonlyArray<DeepReadonly<U>> :
-        T[P] extends ReadonlyArray<infer U> ? ReadonlyArray<DeepReadonly<U>> :
+        T[P] extends Promise<infer R> ? Promise<DeepReadonly<R>> :
+        T[P] extends Set<infer U> | ReadonlySet<infer U> ? ReadonlySet<DeepReadonly<U>> :
+        T[P] extends Map<infer K, infer V> | ReadonlyMap<infer K, infer V> ? ReadonlyMap<K, DeepReadonly<V>> :
+        T[P] extends Array<infer U> | ReadonlyArray<infer U> ? ReadonlyArray<DeepReadonly<U>> :
         T[P] extends {} ? DeepReadonly<T[P]> :
         T[P];
 }
